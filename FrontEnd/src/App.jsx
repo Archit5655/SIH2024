752be901ffcb21c17753b055fa77a3e49e5b9ae3
Ashmit Mehta <amehta.ashmit2@gmail.com>
2024-09-17 17:58:04 +0530

    update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/home'
 import Event from "./pages/Event";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
-import DonationForm from './pages/Donation'
+import Donation from './pages/Donation'
 
 
 function App() {
@@ -15,11 +15,11 @@ function App() {
   return (
  
   <>
-      {/* <Navbar/>
-      <Home/> */}
-      {/* <Event/> */}
-      <DonationForm/>
-      {/* <Footer/> */}
+      <Navbar/>
+      {/* <Home/> */}
+      <Event/>
+      <Donation/>
+      <Footer/>
       
 
   </>
